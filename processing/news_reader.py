a10b63f2918cc34db7905850c244db5db6867da9
tran-quang-thanh <tranquangthanh191@gmail.com>
2020-10-09 15:47:15 +0800

    add word features to data cleaning

--- processing/news_reader.py
+++ processing/news_reader.py
@@ -4,6 +4,7 @@ news_df = reader.get_dataframe('fake_news/train.csv')
 reader.rename_columns(news_df, fake='label', authors='author')
 news_df = reader.extract_columns(news_df)
 
-df = reader.tokenize_columns(news_df, k=10000)
-df = reader.extract_columns(df, ('tokenized text', 'tokenized title', 'authors', 'fake'))
+df = reader.preprocess(news_df, k=10000)
+df = reader.extract_columns(df, ('tokenized text', 'tokenized title', 'authors',
+                                 'citation_count', 'avg sentence len', 'punctuation count', 'fake'))
 reader.write(df, 'news.csv')
--- processing/reader.py
+++ processing/reader.py
@@ -2,35 +2,48 @@ from processing.cleaning import keep_top_k_words, apply_all
 import pandas as pd
 from nltk import FreqDist
 import numpy as np
+import re
 
 pd.set_option("display.max_columns", 30, 'display.expand_frame_repr', False)
 
 
-def get_dataframe(filepath, **kwargs):
-    directory = '../data/' + filepath
-    df = pd.read_csv(directory, **kwargs)
-    return df
+def _avg_sentence_len(text):
+    sentence_list = text.split(". ")
+    total_len = 0
+    for sentence in sentence_list:
+        total_len += len(sentence.split(" "))
+    return total_len / len(sentence_list)
 
 
-def rename_columns(df, **kwargs):
-    columns = {}
-    for key, value in kwargs.items():
-        columns[value] = key
-    df.rename(columns=columns, inplace=True)
+def _punctuation_count(text, punctuations='!?'):
+    sentence_list = text.split(". ")
+    count = 0
+    for sentence in sentence_list:
+        punc_list = [char for char in sentence if char in punctuations]
+        count += len(punc_list)
+    return count
 
 
-def extract_columns(df, columns=('title', 'text', 'authors', 'fake')):
-    return df[list(columns)]
+def _citation_count(text):
+    """
+        Citation with format "<citation>". e.g: "This is a citation"
+    """
+    pattern = r'"[^"]*"'
+    results = re.finditer(pattern, text)
+    citations = [text[match.start(): match.end()] for match in results]
+    citations = list(set(citations))
+    """
+        APA citation: (name, year)
+    """
+    apa_pattern = r'\(([^"\)]*|\bAnonymous\b|"[^"\)]*")(, )([\d]+|n\.d\.|[\d]+[\w])\)'
+    apa_results = re.finditer(apa_pattern, text)
+    apa_citations = [text[match.start(): match.end()] for match in apa_results]
+    apa_citations = list(set(apa_citations))
+
+    return len(citations) + len(apa_citations)
 
 
-def extract_feature(df, **kwargs):
-    condition = []
-    for key, value in kwargs.items():
-        condition.append(df[key] == value)
-    return df[np.bitwise_and.reduce(condition)]
-
-
-def tokenize_columns(df, columns=('text', 'title'), k=None):
+def _tokenize(df, columns=('text', 'title'), k=None):
     notnull_condition = np.array([df[column].notnull() for column in columns])
     df = df[np.bitwise_and.reduce(notnull_condition)]
     df.drop_duplicates()
@@ -50,33 +63,43 @@ def tokenize_columns(df, columns=('text', 'title'), k=None):
     return df2
 
 
+def extract_columns(df, columns=('title', 'text', 'authors', 'fake')):
+    return df[list(columns)]
+
+
+def extract_feature(df, **kwargs):
+    condition = []
+    for key, value in kwargs.items():
+        condition.append(df[key] == value)
+    return df[np.bitwise_and.reduce(condition)]
+
+
+def get_dataframe(filepath, **kwargs):
+    directory = '../data/' + filepath
+    df = pd.read_csv(directory, **kwargs)
+    return df
+
+
+def get_text_features(df):
+    df['citation_count'] = df.apply(lambda row: _citation_count(row['text']), axis=1)
+    df['avg sentence len'] = df.apply(lambda row: _avg_sentence_len(row['text']), axis=1)
+    df['punctuation count'] = df.apply(lambda row: _punctuation_count(row['text']), axis=1)
+
+
+def preprocess(df, columns=('text', 'title'), k=None):
+    df.dropna(subset=list(columns), inplace=True)
+    get_text_features(df)
+    return _tokenize(df, columns, k)
+
+
+def rename_columns(df, **kwargs):
+    columns = {}
+    for key, value in kwargs.items():
+        columns[value] = key
+    df.rename(columns=columns, inplace=True)
+
+
 def write(df, filename, index=False):
     directory = '../cleaned_data/' + filename
     df = df.sample(frac=1).reset_index(drop=True)
     df.to_csv(directory, index=index)
-
-
-
-# news_df = pd.read_csv('../../data/fake_news/train.csv')
-# news_df.rename(columns={'author': 'authors', 'label': 'fake'}, inplace=True)
-# df = news_df[['title', 'text', 'authors', 'fake']]
-# df = df[(df['text'].notnull()) & (df['title'].notnull())]
-
-# df.drop_duplicates()
-#
-# df['tokenized text'] = df['text'].apply(apply_all)
-# df['tokenized title'] = df['title'].apply(apply_all)
-#
-# k = 10000
-# all_words = [word for item in list(df['tokenized text'] + df['tokenized title']) for word in item]
-# fdist = FreqDist(all_words)
-# top_k_words, _ = zip(*fdist.most_common(k))
-# top_k_words = set(top_k_words)
-# df['tokenized text'] = df['tokenized text'].apply(lambda x: keep_top_k_words(x, top_k_words))
-# df['tokenized text'] = df['tokenized text'].apply(lambda text: ' '.join(text))
-# df['tokenized title'] = df['tokenized title'].apply(lambda text: ' '.join(text))
-# cleaned_df = df[['tokenized text', 'tokenized title', 'authors', 'fake']]
-# cleaned_df = cleaned_df.sample(frac=1).reset_index(drop=True)
-# cleaned_df.to_csv('../data/extracted/news.csv', index=False)
-
-
